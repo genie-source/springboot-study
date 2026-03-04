4a0e110bd4eb0f17a6991c5612466c07a90935d2
jinkyung <genie_lee@hufs.ac.kr>
2026-03-04 12:35:42 +0900

    Add DTO to Entity conversion and save to database

---
+++ src/main/java/org/example/dto/ArticleForm.java
@@ -0,0 +1,26 @@
+package org.example.dto;
+
+import org.example.entity.Article;
+
+public class ArticleForm {
+    private Long id;
+    private String title;
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+    public Article toEntity() {
+        return new Article(null, title, content);
+    }
+}
---
+++ src/main/java/org/example/repository/ArticleRepository.java
@@ -0,0 +1,6 @@
+package org.example.repository;
+import org.example.entity.Article;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+}
